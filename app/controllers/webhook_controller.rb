5d5bc30a6be4e063c26d59103758808daa345655
Phil Wilson <pipwilson@users.noreply.github.com>
2015-07-03 22:55:15 +0100

    Update webhook_controller.rb

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -5,10 +5,13 @@ class WebhookController < ApplicationController
 
   # prefers application/json
   def bamboo
+    puts 'processing incoming bamboo payload'
     json = params[:payload]
+    puts 'json: ' + json
     data = JSON.parse(CGI::unescape(json))
+    puts 'data has been parsed'
     notify_slack(data)
-
+    puts 'all done'
     head :ok
   end
 
@@ -19,11 +22,13 @@ class WebhookController < ApplicationController
   end
 
   def notify_slack(data)
+    puts 'in notify slack'
     notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_ENDPOINT'])
     notifier.ping '', attachments: [create_message(data)]
   end
 
   def create_message(data)
+    puts 'in create_message'
     puts JSON.pretty_generate(data)
     puts data['attachments'][0]['color']
     if data['attachments'][0]['color'] == '#ff0000'
