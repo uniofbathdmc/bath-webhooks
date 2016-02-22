903845fa89af201f3447730d6e09dac390095127
tomnatt <github@tomnatt.com>
2016-02-22 16:13:22 +0000

    Changed timeout to 5 mins

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
 
   def build
     if $build_info.present?
-      limit = 1.minutes.ago
+      limit = 5.minutes.ago
       @recent_build = ($build_info[:time] > limit)
     else
       @recent_build = false
