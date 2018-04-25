d0f29df15c7c91f196509263312c9a08e4cde263
Chris Howlett <crh54@bath.ac.uk>
2018-04-25 11:13:47 +0100

    Prototype of dashboard for github repo statuses
    With placeholder styling!

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,26 @@
+require 'github_api_service'
+
 class WelcomeController < ApplicationController
+  REPOS_TO_CHECK = YAML.safe_load(ENV['REPOS_TO_CHECK']).freeze
+
   def index
   end
 
   def build
     # Get any builds from today
     @builds = BuildInfo.where(time: Time.now.all_day).order(time: :desc).limit(5)
+  end
+
+  def repo_statuses
+    @repo_statuses = REPOS_TO_CHECK.map do |repo_hash|
+      details = GithubApiService.repo_details(repo: repo_hash['name'])
+      {
+        name: details[0],
+        descr: details[1],
+        commits: GithubApiService.branch_compare(repo: repo_hash['name'],
+                                                 base: repo_hash['base'],
+                                                 head: repo_hash['head'])
+      }
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,20 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'vcr'
 
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock # or :fakeweb
+end
+
+# Make minitest output look better
+require 'minitest/reporters'
+MiniTest::Reporters.use! [
+  MiniTest::Reporters::DefaultReporter.new(color: true, slow_count: 5),
+  MiniTest::Reporters::JUnitReporter.new
+]
