74d8b072f35e8974239bea5034b680dc6a5cf9fb
Chris Howlett <crh54@bath.ac.uk>
2020-07-14 17:01:43 +0100

    Upgrade to Rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 require_relative 'boot'
 
-# Pick the frameworks you want:
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -13,15 +12,12 @@ module BathWebhooks
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'London'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
   end
 end
